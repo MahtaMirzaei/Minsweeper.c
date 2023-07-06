7e303d0a7e304ff3f5661ad9c8774191a170dc57
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-07-06 16:36:46 +0330

    header's update was forgotten

---
+++ Minsweeper.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<io.h>
+#define Flush fflush(stdin)
+
+#pragma execution_character_set( "utf-8" )
+#pragma warning (disable:4996)
+
+
+
+
